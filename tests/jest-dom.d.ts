7b843c5e0d7881c6f6059d55f4a993eba1683422
myoshizumi <myoshizumi@alumni.berklee.edu>
2026-05-31 14:14:10 +0900

    test(types): fix eslint violations in jest-dom.d.ts

--- tests/jest-dom.d.ts
+++ tests/jest-dom.d.ts
@@ -1,5 +1,6 @@
 import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
 
 declare module 'bun:test' {
-  interface Matchers<T = any> extends TestingLibraryMatchers<any, T> {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Matchers<T = unknown> extends TestingLibraryMatchers<unknown, T> {}
 }
